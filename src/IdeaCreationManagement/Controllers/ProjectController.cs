c7f62e1375fac4a2541b5e0bc7e0a0be9664a693
bpelikan <bartoszpelikan@gmail.com>
2017-05-20 18:34:11 +0200

    Wyświetlanie projektów

---
+++ src/IdeaCreationManagement/Controllers/ProjectController.cs
@@ -0,0 +1,20 @@
+﻿using IdeaCreationManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdeaCreationManagement.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private AppContext _repository = new AppContext();
+
+        // GET: Project
+        public ActionResult List()
+        {
+            return View(_repository.Projects.ToArray());
+        }
+    }
+}
