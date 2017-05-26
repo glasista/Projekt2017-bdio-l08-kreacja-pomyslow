d3ae3a6c9f1bcc2fc7034344d5314309c1515ade
Pawel Pietrasz <szpght@gmail.com>
2017-05-26 21:40:54 +0200

    kontroler uzytkownikow i lista na sztywno #18

---
+++ src/IdeaCreationManagement/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using IdeaCreationManagement.Models;
+using IdeaCreationManagement.ViewModels;
+
+namespace IdeaCreationManagement.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly AppContext _ctx;
+
+        public UsersController(AppContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public ActionResult Index()
+        {
+            var a = new ListUser()
+            {
+                Email = "a@a.pl",
+                EmailConfirmed = false,
+                Id = "abc123",
+                Name = "pawel",
+                Surname = "pietrasz",
+                Roles = "admin",
+            };
+
+            var b = new ListUser()
+            {
+                Email = "a@a.pl",
+                EmailConfirmed = false,
+                Id = "abc1234",
+                Name = "pawel",
+                Surname = "pietrasz",
+                Roles = "pracownik",
+            };
+            return View(new List<ListUser>(){a, b});
+        }
+    }
+}
---
+++ src/IdeaCreationManagement/ViewModels/ListUser.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace IdeaCreationManagement.ViewModels
+{
+    public class ListUser
+    {
+        public string Id { get; set; }
+        [DisplayName("Imię")]
+        public string Name { get; set; }
+        [DisplayName("Nazwisko")]
+        public string Surname { get; set; }
+        [DisplayName("Email")]
+        public string Email { get; set; }
+        [DisplayName("Zatwierdzony")]
+        public bool EmailConfirmed { get; set; }
+        [DisplayName("Role")]
+        public string Roles { get; set; }
+    }
+}
