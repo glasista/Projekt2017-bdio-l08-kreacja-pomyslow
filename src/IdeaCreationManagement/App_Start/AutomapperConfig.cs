dc54381a5f727049f8284641e510473eb2c8726e
Pawel Pietrasz <szpght@gmail.com>
2017-05-26 23:34:42 +0200

    konfiguracja automappera przy starcie

---
+++ src/IdeaCreationManagement/App_Start/AutomapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using IdeaCreationManagement.Models;
+using IdeaCreationManagement.ViewModels;
+
+namespace IdeaCreationManagement.App_Start
+{
+    public class AutomapperConfig
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(Configuration);
+        }
+
+        private static void Configuration(IMapperConfigurationExpression cfg)
+        {
+        }
+    }
+}
