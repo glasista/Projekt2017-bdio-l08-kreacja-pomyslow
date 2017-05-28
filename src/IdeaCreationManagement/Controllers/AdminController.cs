914aa3a0301b5058e86250dd253d3a6d4dfb0521
megamen128 <megamen1285@gmail.com>
2017-05-28 22:26:39 +0200

    Dodany AdminController Wyświetlanie projektów i pracowników

---
+++ src/IdeaCreationManagement/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IdeaCreationManagement.Models;
+using IdeaCreationManagement.Repositories;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+
+
+namespace IdeaCreationManagement.Controllers
+{
+    public class AdminController : Controller
+    {
+        private AppContext db = new AppContext();
+
+        // GET: 
+        [Authorize(Roles = "admin")]
+
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+
+        public ActionResult ViewProjects()
+        {
+
+            var projects = db.Projects.Include(p => p.Assignee).Include(p => p.Author).Include(p => p.Category).Include(p => p.State);
+            return View(projects.ToList());
+          
+        }
+        public ActionResult ChooseEmployee(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Projects = project.Title;
+            var employee = db.Users.Include(p => p.OrganizationalUnit).Include(p => p.Category).Where(p => p.Category == project.Category);
+            return View(employee);
+        }
+    }
+
+
+}
