f4e967dae2c84cdea41bd8df73a151b51659af90
MykolaNykolaichuk <Nykolaichuk.Mykola.M@gmail.com>
2017-05-30 22:35:40 +0200

    #30
    Tworz. Kontrollera powiadomien(Alert)

---
+++ src/IdeaCreationManagement/Controllers/AlertsController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IdeaCreationManagement.Models;
+
+namespace IdeaCreationManagement.Controllers
+{
+    public class AlertsController : Controller
+    {
+        private AppContext db = new AppContext();
+
+        // GET: Alerts
+        public ActionResult Index()
+        {
+            var alerts = db.Alerts.Include(a => a.AuthorOfChange).Include(a => a.Project).Include(a => a.State);
+            return View(alerts.ToList());
+        }
+
+        // GET: Alerts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Alert alert = db.Alerts.Find(id);
+            if (alert == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alert);
+        }
+
+        // GET: Alerts/Create
+        public ActionResult Create()
+        {
+            ViewBag.AuthorOfChangeId = new SelectList(db.Users, "Id", "Name");
+            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Title");
+            ViewBag.StateId = new SelectList(db.States, "Id", "Name");
+            return View();
+        }
+
+        // POST: Alerts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,TimeOfChange,StateId,AuthorOfChangeId,StudentRead,EmployeeRead,ProjectId")] Alert alert)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Alerts.Add(alert);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AuthorOfChangeId = new SelectList(db.Users, "Id", "Name", alert.AuthorOfChangeId);
+            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Title", alert.ProjectId);
+            ViewBag.StateId = new SelectList(db.States, "Id", "Name", alert.StateId);
+            return View(alert);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
