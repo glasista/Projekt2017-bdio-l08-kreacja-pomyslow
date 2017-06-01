8f82cae28467afbe8461977c26e401fceb0916dd
lpast48 <ukaszpasternak@gmail.com>
2017-06-01 22:57:20 +0200

    Poprawa literówek

--- src/IdeaCreationManagement/Models/File.cs
+++ src/IdeaCreationManagement/Models/File.cs
@@ -1,11 +1,19 @@
-﻿namespace IdeaCreationManagement.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IdeaCreationManagement.Models
 {
     public class File
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nazwa")]
         public string Name { get; set; }
         public string DataType { get; set; }
         public byte[] Content { get; set; }
+
+        [Display(Name = "Rozmiar")]
         public long Size { get; set; }
         public int ProjectId { get; set; }
         public Project Project { get; set; }
