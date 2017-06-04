aabb620fe65977ad6ceec613b43d57aa4a675466
tomciodsad <tomcio5346@gmail.com>
2017-06-04 20:11:05 +0200

    Potwierdzenie emaila treść wiadomosci

--- src/IdeaCreationManagement/App_Start/IdentityConfig.cs
+++ src/IdeaCreationManagement/App_Start/IdentityConfig.cs
@@ -34,9 +34,9 @@ namespace IdeaCreationManagement
         {
             #region formatter
             string text = string.Format("Please click on this link to {0}: {1}", message.Subject, message.Body);
-            string html = "Please confirm your account by clicking this link: <a href=\"" + message.Body + "\">link</a><br/>";
+            string html = "Prosze potwierdzić konto poprzez kliknięcie w ";
 
-            html += HttpUtility.HtmlEncode(@"Or click on the copy the following link on the browser:" + message.Body);
+            html += HttpUtility.HtmlEncode(@"link" + message.Body);
             #endregion
 
             MailMessage msg = new MailMessage();
--- src/IdeaCreationManagement/Controllers/AccountController.cs
+++ src/IdeaCreationManagement/Controllers/AccountController.cs
@@ -187,7 +187,7 @@ namespace IdeaCreationManagement.Controllers
                     // Send an email with this link
                     string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    await UserManager.SendEmailAsync(user.Id, "Potwierdz konto", "    <a href=\"" + callbackUrl + "\">here</a>");
 
                     return RedirectToAction("RegisterStudentStepTwo", "Account");
                 }
