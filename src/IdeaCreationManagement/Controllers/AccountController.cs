c3ba009369b0b09f6511800882c39bd85894b017
Pawel Pietrasz <szpght@gmail.com>
2017-06-14 22:22:58 +0200

    blokada logowania na niepotwierdzone konto

--- src/IdeaCreationManagement/Controllers/AccountController.cs
+++ src/IdeaCreationManagement/Controllers/AccountController.cs
@@ -75,6 +75,14 @@ namespace IdeaCreationManagement.Controllers
                 return View(model);
             }
 
+            // sprawdzenie czy konto usera jest potwierdzone
+            var user = UserManager.FindByName(model.Email);
+            if (user != null && !user.EmailConfirmed)
+            {
+                ModelState.AddModelError("", "Konto nie jest potwierdzone");
+                return View(model);
+            }
+
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
